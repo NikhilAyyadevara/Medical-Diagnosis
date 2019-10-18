976352c3c4e07b86cfdc8485d10c5db87eb42294
avsnikhil <nikhil.ayyadevara@gmail.com>
2019-10-18 22:39:37 +0530

    Minor changes

--- Parser.cpp
+++ Parser.cpp
@@ -39,13 +39,13 @@ network read_network(string s)
 					for(int i=0;i<4;i++)
 					{
 						ss2>>temp;
-						cout<<temp<<"\n";
+						// cout<<temp<<"\n";
 					}
 					values.clear();
 					while(temp.compare("};")!=0)
 					{
 						values.push_back(temp);
-						cout<<temp<<"\n";
+						// cout<<temp<<"\n";
 						ss2>>temp;
 					}
 					Graph_Node new_node(name,values.size(),values);
