d0e4790702bb599c09cd78538374faae740a8da5
avsnikhil <nikhil.ayyadevara@gmail.com>
2019-10-20 18:37:57 +0530

    Added -std=c++11

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ -O3 dataParser.cpp Graph_Node.cpp  main.cpp  network.cpp  out.cpp  Parser.cpp -o bayesnet
+g++ -O3 -std=c++11 dataParser.cpp Graph_Node.cpp  main.cpp  network.cpp  out.cpp  Parser.cpp -o bayesnet
